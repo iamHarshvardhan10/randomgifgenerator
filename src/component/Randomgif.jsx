ada6fca014cc432e53158029dd3c4594eb226abe
Harshvardhan <shreeharshpb11@gmail.com>
2024-04-30 00:28:59 +0530

    complete meme generator functionality

--- src/component/Randomgif.jsx
+++ src/component/Randomgif.jsx
@@ -1,19 +1,24 @@
 import axios from "axios";
 import { useEffect, useState } from "react"
+import Spinner from "./Spinner";
+
 
 
 const API_KEY = 'LJJwB7XtlNjPybcgINxzFp2c1AvzYf4d';
 
 const Randomgif = () => {
     const [gif , setGif] = useState('')
+    const [loading , setLoading] = useState(false)
     
     const getRandomGif = async () => {
+      setLoading(true)
       const apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
       const {data} = await axios.get(apiUrl)
       console.log(data)
       const imageSource = data.data.images.downsized.url;
       // console.log(imageSource)
       setGif(imageSource)
+      setLoading(false)
     }
 
     useEffect(() => {
@@ -27,7 +32,10 @@ const Randomgif = () => {
   return (
     <div className="p-[20px] mt-[20px] w-[40%] h-auto bg-green-500 border border-black rounded-xl">
         <h1 className="text-2xl uppercase p-2 font-bold text-center mb-2 bg-yellow-200 border border-yellow-800 rounded-lg">Random GIF</h1>
-        <img src={gif} alt="" className="w-full h-[350px]"/>
+        {
+          loading ? (<Spinner/>) : (<img src={gif} alt="" className="w-full h-[350px]"/>)
+        }
+        
         <button className="p-[10px] w-full font-bold text-xl rounded-md border border-black uppercase mt-[20px] bg-red-500 " onClick={generator}>Generate</button>
     </div>
   )
